ea539873cec8b1148e660d827ea6219f2d404b49
CircArgs <quebecname@gmail.com>
2019-11-29 19:45:06 -0500

    helper info into action entrypoint

--- .github/actions/testing/entrypoint.sh
+++ .github/actions/testing/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+printenv
 pip3 install pipenv;
 pipenv install;
 pipenv run python -m pytest --cov=. >> $GITHUB_WORKSPACE/coverage_summary
+cat $GITHUB_WORKSPACE/coverage_summary
