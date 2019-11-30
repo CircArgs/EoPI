3994b47c9a5fae6a9d591743c78a882246f8c22e
CircArgs <quebecname@gmail.com>
2019-11-29 20:20:52 -0500

    more action testing in entrypoint

--- .github/actions/testing/entrypoint.sh
+++ .github/actions/testing/entrypoint.sh
@@ -2,5 +2,8 @@
 printenv
 pip3 install pipenv;
 pipenv install;
-pipenv run python -m pytest --cov=.
-git status
+pipenv run python -m pytest --cov=. >> $GITHUB_WORKSPACE/coverage_summary
+pipenv run python .github/actions/testing/make_coverage_badge.py
+cat $GITHUB_WORKSPACE/coverage_summary
+tree
+
