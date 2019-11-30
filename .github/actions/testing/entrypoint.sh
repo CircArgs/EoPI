ad5c0152c3ebe55b159c3f08c7e919ce0dc15385
CircArgs <quebecname@gmail.com>
2019-11-29 20:34:00 -0500

    push from actions

--- .github/actions/testing/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-printenv
-pip3 install pipenv;
-pipenv install;
-pipenv run python -m pytest --cov=. >> $GITHUB_WORKSPACE/coverage_summary
-pipenv run python .github/actions/testing/make_coverage_badge.py
-cat $GITHUB_WORKSPACE/coverage_summary
-tree
-
--- README.md
+++
@@ -1,6 +0,0 @@
-# Solutions to Elements of Programming Interviews in Python
-
-![](https://github.com/CircArgs/EoPI/workflows/test/badge.svg)
-
-
-![](https://github.com/CircArgs/EoPI/blob/master/.github/actions/testing/badge_template.svg)
