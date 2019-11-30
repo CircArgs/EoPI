6bfd399f380aa13864786e634868872193332d91
CircArgs <quebecname@gmail.com>
2019-11-29 20:46:21 -0500

    entrypoint for testing workflow attempts to push to branch for badges

--- .github/actions/testing/entrypoint.sh
+++ .github/actions/testing/entrypoint.sh
@@ -4,6 +4,14 @@ pip3 install pipenv;
 pipenv install;
 pipenv run python -m pytest --cov=. >> $GITHUB_WORKSPACE/coverage_summary
 pipenv run python .github/actions/testing/make_coverage_badge.py
+mkdir badges
+cd badges
+git clone https://github.com/CircArgs/EoPI.git .
+git checkout badges
+cp $GITHUB_WORKSPACE/coverage_badge.svg .
+git config remote.origin.url https://CircArgs:$GITPASS@github.com/CircArgs/EoPI.git
+git add .
+git commit -m "push from action"
+
 cat $GITHUB_WORKSPACE/coverage_summary
-tree
 
