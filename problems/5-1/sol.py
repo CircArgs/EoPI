b53f37fda1b8ddb382265da9738d78af0346bc51
CircArgs <quebecname@gmail.com>
2019-12-01 17:17:07 -0500

    wrote solution for 5-1

---
+++ problems/5-1/sol.py
@@ -0,0 +1,71 @@
+def swap(l, a, b):
+    temp = l[a]
+    l[a] = l[b]
+    l[b] = temp
+
+
+def shift_elements(a, s, e, p):
+
+    if s >= e:
+        return
+    assert s <= p <= e, "Invalid Pivot"
+    assert s < e, f"Invalid start: {s} and end: {e}. Must have Start<=End."
+    c = 0
+    i = 0
+    bp = p
+    while i < p:
+        if a[i] > a[p]:
+            a.insert(e + 1, a[i])
+            del a[i]
+            p -= 1
+            bp -= 1
+            c += 1
+        elif a[i] == a[p]:
+            swap(a, i, p - 1)
+            p -= 1
+        else:
+            i += 1
+    left_end = p
+
+    i = -c + e
+    p = bp
+    while i > p:
+        if a[i] < a[p]:
+            a.insert(s, a[i])
+            del a[i + 1]
+            p += 1
+        elif a[i] == a[p]:
+            swap(a, i, p + 1)
+            p += 1
+        else:
+            i -= 1
+
+    left_end += p - bp
+    left_start = s
+    right_end = e
+    right_start = p
+
+    left_end -= 1
+    right_start += 1
+    shift_elements(a, left_start, left_end, left_start + (left_end - left_start) // 2)
+    shift_elements(
+        a, right_start, right_end, right_start + (right_end - right_start) // 2
+    )
+
+    return a
+
+
+def solution(l):
+    return shift_elements(l, 0, len(l) - 1, len(l) // 2)
+
+
+def extra_space_sol(a):
+    if not a:
+        return []
+    p = a[len(a) // 2]
+    return (
+        extra_space_sol([i for i in a if i < p])
+        + [i for i in a if i == p]
+        + extra_space_sol([i for i in a if i > p])
+    )
+
---
+++ problems/5-1/test_5-1.py
@@ -0,0 +1,15 @@
+import pytest
+import random
+
+from .sol import solution, extra_space_sol
+
+
+@pytest.fixture
+def random_list():
+    length = random.randint(0, 100)
+    return [random.randint(0, 100) for _ in range(length)]
+
+
+def test(random_list):
+    assert sorted(random_list) == extra_space_sol(random_list) == sol(random_list)
+
