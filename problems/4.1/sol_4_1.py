dba225fedd764b77dad5d65754decf505d4ce3e9
CircArgs <quebecname@gmail.com>
2019-12-02 09:18:41 -0500

    4.1 new cache, commented all code, new ones func

--- problems/4.1/sol_4_1.py
+++ problems/4.1/sol_4_1.py
@@ -1,35 +1,32 @@
 """EoPI pg 24
-The parity of a binary word is 1
-if the number of 1s in the word is odd; otherwise, it is 0.
-For
-example, the parity of 1011 is 1, and the parity of 10001000 is 0. Parity checks are used to detect
-single bit errors in data storage and communication. It is fairly straightforward to write code that
-computes the parity of a single 64-bit word.
+The parity of a binary word is 1 if the number of 1s in the word is odd; otherwise, it is 0.
+For example, the parity of 1011 is 1, and the parity of 10001000 is 0. 
+Parity checks are used to detect single bit errors in data storage and communication. 
+It is fairly straightforward to write code that computes the parity of a single 64-bit word.
 How would you compute the parity of a very large number of 64-bit words?
-Hint: Use a lookup table, but don't use
-2^64
-entries!
+Hint: Use a lookup table, but don't use 2^64 entries!
 """
 
-# we use a dictionary (hash table) as a cache. we cache words (bit sequences) and subwords we haven't seen before.
+# we create a hashtable based cache by extending defaultdict. we cache words (bit sequences) and subwords we haven't seen before.
 # the cache could be modified to use a least recently used (LRU) cache with a max size
-cache = {}
+from collections import defaultdict
 
 
-def cache_checker(n: int) -> int:
-    v = cache.pop(n, None)
-    if v != None:
-        cache[v] = v
-        return v
-    else:
-        v = parity(n)
-    cache[n] = v
-    return v
+class Cache(defaultdict):
+    # If dict lookup fails this will be called
+    def __missing__(self, key):
+        # Insert the parity of the key into the cache dict and return it
+        value = _parity(key)
+        self[key] = value
+        return value
 
+
+cache = Cache()
 
 # this is barely slower
-def ones(n):
-    return 2 ** n.bit_length() - 1
+# def ones(n):
+#     '''generate largest number of bit length n'''
+#     return 2 ** n.bit_length() - 1
 
 
 # than this:
@@ -39,16 +36,21 @@ def ones(n):
 #     return int("1" * n.bit_length(), 2)
 # but uses less memory in avoiding strings
 
+# and this one is fastest of the three
+def ones(n):
+    """generate largest number of bit length n"""
+    return (1 << n.bit_length()) - 1
+
 
 def _parity(n: int) -> int:
     bl = n.bit_length()
     if bl < 2:
         return n
     if bl % 2:
-        return ((n & 1) + cache_checker(n >> 1)) % 2
+        return ((n & 1) + cache[n >> 1]) % 2
     else:
         temp = n >> (bl // 2)
-        return cache_checker((ones(temp) & n) ^ temp)
+        return cache[(ones(temp) & n) ^ temp]
 
 
 def parity(n: int) -> int:
