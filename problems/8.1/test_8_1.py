e5537a336abd71c183f24fc9e797554dcf3f94c5
CircArgs <quebecname@gmail.com>
2019-12-04 19:51:30 -0500

    finished test for 8.1

--- problems/8.1/test_8_1.py
+++ problems/8.1/test_8_1.py
@@ -7,11 +7,11 @@ def test():
     for _ in range(1000):
         s = Stack()
         m = -float("inf")
-        for i in range(random.randint(0, 10000)):
-            ri = random.randint(-10000, 10000)
-            s.push(ri)
-            if ri > m:
-                m = ri
-            if random.randint(0, 1) and len(s) > 1:
-                s.pop()
+        n = random.randint(0, 10000)
+        temp = [None] * n
+        for i in range(n):
+            v = random.randint(-10000, 10000)
+            if m < i:
+                m = i
+            s.push(i)
             assert s.max() == m
